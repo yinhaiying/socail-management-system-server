33febbae70966272b58002b851095bcffdf23d7f
yinhaiying <yinhaiying@58.com>
2020-06-07 16:51:52 +0800

    连接mongodb数据库

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const koa = require('koa');
 const Router = require('koa-router');
-
-
+const mongoose  = require('mongoose');
+const config = require('./config/keys.js');
 // 实例化koa
 
 const app = new koa();
 const router = new Router();
 
 // 路由
-
 router.get('/', async ctx => {
   ctx.body = 'hello,world'
+})
+
+
+
+// 连接数据库  这里使用的是mongodb aws免费的数据库 (https://cloud.mongodb.com/)
+const db = mongoose.connect(config.mongoURI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('mongodb 连接成功');
+}).catch((err) => {
+  console.log('mongodb连接失败:'+err);
 })
 
 
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // mongodb连接地址
+   mongoURI:'mongodb+srv://haiyingsitan:AAA123456@cluster0-vnvhf.mongodb.net/<dbname>?retryWrites=true&w=majority' 
+}
